b1ce96e7ab85f1b2dca70b3f35f73ba99721a6cc
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-24 02:09:47 +0600

    create verifiey admin functiont to check admin

--- index.js
+++ index.js
@@ -39,6 +39,18 @@ async function run(){
         const userCollection = client.db('motion-car-parts').collection('user');
         const itemCollection = client.db("motion-car-parts").collection("item");
 
+      // verify Admin function:
+        const verifyAdmin = async (req, res, next) => {
+
+          const requester = req.decoded.email;
+          const requesterAccount = await userCollection.findOne({email: requester});
+            if (requesterAccount.role === 'admin'){
+              next()
+            }
+            else{
+              res.status(403).send({message: 'forbidden'})
+            }
+        }
 
 
         // Product all api
@@ -77,7 +89,7 @@ async function run(){
           })
 
         // create admin user
-        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async(req, res)=>{
+        app.put('/user/admin/:email', verifyJWT, async(req, res)=>{
           const email = req.params.email;
           const filter = {email: email};
           const updateDoc = {
