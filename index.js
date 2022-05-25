db9c46bec1d8a32dfee04762b31c9a22e170a680
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-26 00:58:26 +0600

    Create get api to load any specified order with id

--- index.js
+++ index.js
@@ -102,12 +102,23 @@ async function run(){
         }
       });
 
+      // id to find order api
+      app.get('/order/:id',verifyJWT, async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id:ObjectId(id)} ;
+        const order = await orderCollection.findOne(query)
+        res.send(order)
+      });
+
       //Order Api
-      app.post('/order', async(req,res)=>{
+      app.post('/order', verifyJWT, async(req,res)=>{
         const newOrder = req.body ;
         const result = await orderCollection.insertOne(newOrder)
         res.send(result)
       })
+
+      // user Order delete api
+      
 
 
       // reviews api
